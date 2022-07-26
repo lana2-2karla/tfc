a7c6606e73d3afd317b48f2e6efb799787c86b59
Allana Karla <allanakarla1702@gmail.com>
2022-07-26 18:00:47 -0300

    :sparkles: feat: criando model - User

---
+++ app/backend/src/database/models/user.ts
@@ -0,0 +1,40 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  id: number;
+  userName: string;
+  role: string;
+  email: string;
+  password: string;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  userName: {
+    type: STRING(255),
+    allowNull: false,
+  },
+  role: {
+    type: STRING(255),
+    allowNull: false,
+  },
+  email: {
+    type: STRING(255),
+    allowNull: false,
+  },
+  password: {
+    type: STRING(255),
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'user',
+  timestamps: false });
+
+export default User;
