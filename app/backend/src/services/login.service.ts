3a27f83710ee5c478cd1edf97a6436da2ec88b69
Allana Karla <allanakarla1702@gmail.com>
2022-08-01 15:24:26 -0300

    :recycle: refactor: refatora service de login

--- app/backend/src/services/login.service.ts
+++ app/backend/src/services/login.service.ts
@@ -1,22 +1,28 @@
 /* eslint-disable class-methods-use-this */
-import { Ilogin } from '../interfaces/interface';
+import * as bcrypt from 'bcryptjs';
+import { Ilogin, Itoken } from '../interfaces/interface';
 import User from '../database/models/user';
 import TokenGenerate from '../utils/jwtToken';
+import HttpException from '../utils/http.exception';
 
 class UserTokenService {
-  public async authLogin(userData: Ilogin) {
+  public async authLogin(userData: Ilogin): Promise<Itoken> {
     const user = await User.findOne({
-      attributes: { exclude: ['password'] },
       where: {
         email: userData.email,
       },
     });
 
-    if (user === null) throw new Error('invalid user');
+    if (!user) throw new HttpException(401, 'Incorrect email or password');
+    const { password, email } = user;
+    const passwordHash = await bcrypt.compare(userData.password, password);
+    if (!passwordHash) throw new HttpException(401, 'Incorrect email or password');
     const newToken = new TokenGenerate();
-    const token = newToken.generateJwtToken(user.email);
+    const token = newToken.generateJwtToken(email);
     return { token };
   }
 }
 
 export default UserTokenService;
+
+// ref bcrypt - https://www.npmjs.com/package/bcrypt
