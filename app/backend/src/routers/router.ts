1526a890cb2433629fd0489ba5435845ad675e8b
Allana Karla <allanakarla1702@gmail.com>
2022-08-01 19:00:49 -0300

    :sparkles: feat: cria rota /teams - método GET

--- app/backend/src/routers/router.ts
+++ app/backend/src/routers/router.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from 'express';
 import loginValidate from '../middlewares/login.validate';
 import UserTokenController from '../controllers/login.controller';
+import TeamsController from '../controllers/teams.controller';
 
 const routers: Router = Router();
 
@@ -15,5 +16,7 @@ routers.get(
   '/login/validate',
   (req: Request, res: Response) => userTokenController.UserRole(req, res),
 );
+
+routers.get('/teams', (req: Request, res: Response) => TeamsController.getTeams(req, res));
 
 export default routers;
