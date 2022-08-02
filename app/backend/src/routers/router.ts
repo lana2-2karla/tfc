1739eb31e9f661a610c386991144f9e74fb53bee
Allana Karla <allanakarla1702@gmail.com>
2022-08-02 18:39:28 -0300

    :sparkles: feat: cria rota /matches - método POST

--- app/backend/src/routers/router.ts
+++ app/backend/src/routers/router.ts
@@ -3,6 +3,7 @@ import loginValidate from '../middlewares/login.validate';
 import UserTokenController from '../controllers/login.controller';
 import TeamsController from '../controllers/teams.controller';
 import MatchesController from '../controllers/matches.controller';
+import authTokenMiddleware from '../middlewares/token.validate';
 
 const routers: Router = Router();
 
@@ -24,5 +25,11 @@ routers.get('/teams', (req: Request, res: Response) => TeamsController.getTeams(
 routers.get('/teams/:id', (req: Request, res: Response) => TeamsController.getTeamsById(req, res));
 
 routers.get('/matches', (req: Request, res: Response) => MatchesController.getMatches(req, res));
+
+routers.post(
+  '/matches',
+  authTokenMiddleware.authToken,
+  (req: Request, res: Response) => MatchesController.createNewMatch(req, res),
+);
 
 export default routers;
