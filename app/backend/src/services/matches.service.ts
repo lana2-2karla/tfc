dd7f2f34c22363ec8c54401c9ea85a8a433a9b0a
Allana Karla <allanakarla1702@gmail.com>
2022-08-02 18:37:25 -0300

    :sparkles: feat: cria método createNewMatch

--- app/backend/src/services/matches.service.ts
+++ app/backend/src/services/matches.service.ts
@@ -1,18 +1,36 @@
+import { IMatch } from '../interfaces/interface';
 import Match from '../database/models/matches';
+import HttpException from '../utils/http.exception';
+import Team from '../database/models/teams';
 
 class MatchesService {
   public static async getMatches(): Promise<Match[]> {
-    const matches = Match.findAll();
+    const matches = await Match.findAll();
     return matches;
   }
 
   public static async getMatchesByInProgress(inProgress: boolean): Promise<Match[]> {
-    const matches = Match.findAll({
+    const matches = await Match.findAll({
       where: {
         inProgress,
       },
     });
     return matches;
+  }
+
+  public static async createNewMatch(matchData: IMatch, inProgress: boolean) {
+    const { homeTeam, awayTeam } = matchData;
+    if (homeTeam === awayTeam) {
+      throw new HttpException(401, 'It is not possible to create a match with two equal teams');
+    }
+    const isRepeatedHome = await Team.findOne({ where: { id: homeTeam } });
+
+    const isRepeatedAway = await Team.findOne({ where: { id: awayTeam } });
+    if (!isRepeatedHome || !isRepeatedAway) {
+      throw new HttpException(401, 'There is no team with such id!');
+    }
+    const newMatch = await Match.create({ ...matchData, inProgress });
+    return newMatch;
   }
 }
 
